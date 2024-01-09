44f22633b82fec9f15f111ca4c7ef598b000016b
MaksimYurau <2004softwaredeveloper@gmail.com>
2024-01-10 01:48:48 +0300

    Updated project. Updated ConsoleApplicationTask12.cpp.

--- ConsoleApplicationTask12/ConsoleApplicationTask12.cpp
+++ ConsoleApplicationTask12/ConsoleApplicationTask12.cpp
@@ -8,24 +8,16 @@
 int main() {
     setlocale(0, "ru");
 
-    int arr1[] = { 1, 2, 3, 4, 5 };
-    int arr2[] = { 6, 7, 8, 9, 10 };
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    OneDimensionalArray array1(arr, 10);
 
-    OneDimensionalArray array1(arr1, 5);
-    OneDimensionalArray array2(arr2, 5);
-
-    OneDimensionalArray array3 = array1 + array2;
     std::cout << "Исходный одномерный массив: " << std::endl;
-    array3.printArray();
-
-    array1.removeVowels();
+    array1.printArray();
 
     std::cout << "Одномерный массив без первых пяти элементов: " << std::endl;
 
+    array1.removeFirstFiveElements();
     array1.printArray();
-
-    array2.removeFirstFiveElements();
-    array2.printArray();
 
     std::cout << std::endl;
 
